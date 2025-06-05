951b8f2f6564ed1cf8ed6e3e9997a5aba7400d69
yangxiao <yangxiao@outloo.com>
2025-06-05 22:36:39 +0800

    添加了密码存储到数据库的异或修改处理

--- chatdemo/global.cpp
+++ chatdemo/global.cpp
@@ -5,3 +5,18 @@ std::function<void(QWidget*)> repolish =[](QWidget* w)
     w->style()->unpolish(w);
     w->style()->polish(w);
 };
+
+
+std::function<QString(QString)>xorString=[](QString input)//简单加密
+{
+    QString result = input;
+    int length =input.length();
+    length = length % 255;
+    for (int i = 0; i < length; ++i)
+    {
+        //每个字符都进行异或操作
+        // 这里假设字符都是ASCII。因此直接转换成QChar
+        result[i] = QChar(static_cast<ushort>(input[i].unicode()^static_cast<ushort>(length)));
+    }
+    return result;
+};
