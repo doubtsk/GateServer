8ecd42f6a34685899e81e540eebac782279c191e
yangxiao <yangxiao@outloo.com>
2025-06-07 23:26:36 +0800

    相关Mysql文件添加检查邮箱和密码是否匹配的逻辑表现

--- GateServe/MysqlMgr.h
+++ GateServe/MysqlMgr.h
@@ -7,11 +7,10 @@ class MysqlMgr :public Singleton<MysqlMgr>
 public:
 	~MysqlMgr();
 	int RegUser(const std::string& name, const std::string& email, const std::string& pwd);
-	/*
+	
 	bool CheckEmail(const std::string& name, const std::string& email);
-	bool updatePwd(const std::string& name, const std::string& email);
-	bool CheckPwd(const std::string& name, const std::string& pwd, UserInfo& userInfo);
-	*/
+	bool UpdatePwd(const std::string& name, const std::string& email);
+	//bool CheckPwd(const std::string& name, const std::string& pwd, UserInfo& userInfo);
 private:
 	MysqlMgr();
 	MysqlDao _dao;
