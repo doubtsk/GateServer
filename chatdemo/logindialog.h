15a9f968ff0f50b51ecca02eb5e7f3df5540fa03
yangxiao <yangxiao@outloo.com>
2025-06-13 11:07:14 +0800

    完善登录逻辑

--- chatdemo/logindialog.h
+++ chatdemo/logindialog.h
@@ -1,6 +1,7 @@
 #ifndef LOGINDIALOG_H
 #define LOGINDIALOG_H
 
+#include "global.h"
 #include <QDialog>
 
 namespace Ui {
@@ -16,13 +17,28 @@ public:
     ~loginDialog();
 
 private:
+    void initHttpHandlers();
+    void initHead();
     Ui::loginDialog *ui;
-
+    bool checkUserValid();
+    void AddTipErr(TipErr te,QString tips);
+    void DelTipErr(TipErr te);
+    bool checkPwdValid();
+    bool enableBtn(bool enabled);
+    QMap<TipErr,QString> _tip_errs;
+    QMap<ReqId,std::function<void (const QJsonObject&)>>_handlers;
+    int _uid;
+    QString _token;
 public slots:
     void slot_forget_pwd();
+    void showTip(QString str,bool b_ok);
 signals:
     void switchregister();
     void switchReset();
+    void sig_connect_tcp(ServerInfo);
+private slots:
+    void on_sign_btn_clicked();
+    void slot_login_mod_finish(ReqId id,QString res,ErrorCodes err);
 };
 
 #endif // LOGINDIALOG_H
