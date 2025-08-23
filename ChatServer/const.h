3480eee416878a16f7ea3c5a7ae94e123fbd9ef2
yangxiao <yangxiao@outloo.com>
2025-08-24 00:48:55 +0800

    添加库支持，以及添加报错编号，哦对了，不要添加莫名其妙的代码

--- ChatServer/const.h
+++ ChatServer/const.h
@@ -1,4 +1,30 @@
 #pragma once
+#include <boost/beast/http.hpp>
+#include <boost/beast.hpp>
+#include <boost/asio.hpp>
+#include <memory>
+#include <iostream>
+#include <unordered_map>
+#include <json/json.h>
+#include <json/value.h>
+#include <json/reader.h>
+#include "Singleton.h"
+#include <assert.h>
+#include <queue>
+#include <jdbc/mysql_driver.h>
+#include <jdbc/mysql_connection.h>
+#include <jdbc/cppconn/prepared_statement.h>
+#include <jdbc/cppconn/resultset.h>
+#include <jdbc/cppconn/statement.h>
+#include <jdbc/cppconn/exception.h>
+#include <iostream>
+#include <functional>
+#include <grpcpp/grpcpp.h>
+#include "message.grpc.pb.h"
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include<functional>
 #define MAX_LENGTH  1024*2
 //头部总长度
 #define HEAD_TOTAL_LEN 4
@@ -11,5 +37,36 @@
 
 
 enum MSG_IDS {
-	MSG_HELLO_WORD = 1001
+	MSG_CHAT_LOGIN = 1005, //用户登陆
+	MSG_CHAT_LOGIN_RSP = 1006, //用户登陆回包
+	ID_SEARCH_USER_REQ = 1007, //用户搜索请求
+	ID_SEARCH_USER_RSP = 1008, //搜索用户回包
+	ID_NOTIFY_ADD_FRIEND_REQ = 1011,  //通知用户添加好友申请
 };
+class Defer
+{
+public:
+	Defer(std::function<void()> func) :func_(func) {}
+	~Defer()
+	{
+		func_();
+	}
+private:
+	std::function<void()> func_;
+};
+
+enum ErrorCodes {
+	Success = 0,
+	Error_Json = 1001,  //Json解析错误
+	RPCFailed = 1002,  //RPC请求错误
+	VarifyExpired = 1003, //验证码过期
+	VarifyCodeErr = 1004, //验证码错误
+	UserExist = 1005,       //用户已经存在
+	PasswdErr = 1006,    //密码错误
+	EmailNotMatch = 1007,  //邮箱不匹配
+	PasswdUpFailed = 1008,  //更新密码失败
+	PasswdInvalid = 1009,   //密码更新失败
+	RPCGetFailed = 1010,  //获取rpc请求失败
+	UidInvalid = 1011,  //uid无效
+};
+//std::function<void()>func_;
